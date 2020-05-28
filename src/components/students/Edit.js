b0d0aedfc30e2429db04314174f26b4acf2598b4
dvega1193 <dvega1193@gmail.com>
2020-05-28 14:03:57 -0600

    dd

--- src/components/students/Edit.js
+++ src/components/students/Edit.js
@@ -1,114 +1,122 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import { Redirect } from 'react-router';
-import NavBar from '../NavBar';
+import React, { Component } from "react";
+import axios from "axios";
+import { Redirect } from "react-router";
+import NavBar from "../NavBar";
 export default class Edit extends Component {
+  constructor(props) {
+    super(props);
+    this.onChangeFirstName = this.onChangeFirstName.bind(this);
+    this.onChangeLastName = this.onChangeLastName.bind(this);
+    this.onChangeEmail = this.onChangeEmail.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
 
-    constructor(props){
-        super(props);
-        this.onChangeFirstName = this.onChangeFirstName.bind(this);
-        this.onChangeLastName = this.onChangeLastName.bind(this);
-        this.onChangeEmail = this.onChangeEmail.bind(this);
-        this.onSubmit = this.onSubmit.bind(this);
-  
-        this.state = {
-             first_name: '',
-             last_name: '',
-             email: '',
-             redirect: false
-        }
-     }
- 
+    this.state = {
+      first_name: "",
+      last_name: "",
+      email: "",
+      redirect: false,
+    };
+  }
 
-     componentDidMount(){
-        axios.get('http://localhost/ReactPHPCRUD/getById.php?id='+this.props.match.params.id)
-        .then(response => {
-            this.setState({
-                first_name: response.data.fName,
-                last_name: response.data.lName,
-                email: response.data.email});
-        })
-        .catch(function(error){
-            console.log(error);
-        })
-     }
-
-     onChangeFirstName(e){
+  componentDidMount() {
+    axios
+      .get(
+        "http://localhost/ReactPHPCRUD/getById.php?id=" +
+          this.props.match.params.id
+      )
+      .then((response) => {
         this.setState({
-          first_name: e.target.value
+          first_name: response.data.fName,
+          last_name: response.data.lName,
+          email: response.data.email,
         });
-      }
-  
-      onChangeLastName(e){
-        this.setState({
-          last_name: e.target.value
-        });
-      }
-  
-      onChangeEmail(e){
-        this.setState({
-          email: e.target.value
-        });
-      }
- 
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
 
-      onSubmit(e){
-        e.preventDefault();
+  onChangeFirstName(e) {
+    this.setState({
+      first_name: e.target.value,
+    });
+  }
 
-        const obj = {
-           //id = this.state.sId, 
-           first_name: this.state.first_name,
-           last_name: this.state.last_name,
-           email: this.state.email
-        };
+  onChangeLastName(e) {
+    this.setState({
+      last_name: e.target.value,
+    });
+  }
 
+  onChangeEmail(e) {
+    this.setState({
+      email: e.target.value,
+    });
+  }
 
-        axios.post('http://localhost/ReactPHPCRUD/update.php?id='+this.props.match.params.id, obj)
-        .then(
-            res => console.log(res.data),
-            this.setState({ redirect:true })
-            ); 
-      }
+  onSubmit(e) {
+    e.preventDefault();
 
-     render(){
+    const obj = {
+      //id = this.state.sId,
+      first_name: this.state.first_name,
+      last_name: this.state.last_name,
+      email: this.state.email,
+    };
 
-        const {redirect} = this.state;
-        if(redirect){
-            return <Redirect to='/students'/>
-        }
+    axios
+      .post(
+        "http://localhost/ReactPHPCRUD/update.php?id=" +
+          this.props.match.params.id,
+        obj
+      )
+      .then((res) => console.log(res.data), this.setState({ redirect: true }));
+  }
 
-         return(
-          
-       <div>   
-          <NavBar/>   
+  render() {
+    const { redirect } = this.state;
+    if (redirect) {
+      return <Redirect to="/students" />;
+    }
 
-            <form onSubmit={this.onSubmit}>
-              <div className="form-group">
-                <label>Primer nombre:</label>
-                <input type="text" className="form-control" 
-                value={this.state.first_name}
-                onChange={this.onChangeFirstName} />
-              </div>
- 
-              <div className="form-group">
-                <label>Apellido:</label>
-                <input type="text" className="form-control"
-                value={this.state.last_name}
-                onChange={this.onChangeLastName} />
-              </div>
- 
-              <div className="form-group">
-                <label>Correo:</label>
-                <input type="text" className="form-control"
-                 value={this.state.email}
-                 onChange={this.onChangeEmail}/>
-              </div>
- 
-           
-              <input type="submit" className="btn btn-primary" value="Actualizar"/>
-            </form>
-          </div>  
-         );
-     }
+    return (
+      <div>
+        <NavBar />
 
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label>Primer nombre:</label>
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.first_name}
+              onChange={this.onChangeFirstName}
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Apellido:</label>
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.last_name}
+              onChange={this.onChangeLastName}
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Correo:</label>
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.email}
+              onChange={this.onChangeEmail}
+            />
+          </div>
+
+          <input type="submit" className="btn btn-primary" value="Actualizar" />
+        </form>
+      </div>
+    );
+  }
 }
